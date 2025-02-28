55295c9fa1c3fd9c5cd347a957c2dd6adbe21a05
Alex Woodhouse <awoodhouse@transurban.com>
2025-02-28 14:31:19 +1100

    feat: update Lambda function configuration and required provider version

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 resource "aws_lambda_function" "hello_world" {
-    filename         = "lambda-output/hello_world.zip"
-    function_name    = "hello_world"
+    filename         = "${path.module}/lambda-output/hello_world.zip"
+    function_name    = "${var.environment}-hello_world"
     role             = aws_iam_role.lambda_exec.arn
     handler          = "index.handler"
-    runtime          = "nodejs14.x"
-    source_code_hash = filebase64sha256("lambda-output/hello_world.zip")
+    runtime          = "nodejs22.x"
+    source_code_hash = filebase64sha256("${path.module}/lambda-output/hello_world.zip")
 }
 
 resource "aws_iam_role" "lambda_exec" {
-    name = "hello_world_lambda_exec_role"
-
+    
     assume_role_policy = jsonencode({
         Version = "2012-10-17"
         Statement = [
@@ -22,6 +21,7 @@ resource "aws_iam_role" "lambda_exec" {
             }
         ]
     })
+
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
